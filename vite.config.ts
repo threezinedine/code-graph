b77e4f8807b04a5466bd0f6994fd6ba016834be9
Admin <threezinedine@gmail.com>
2026-04-25 23:02:02 +0700

    [configure] setup the project structure

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import path from "node:path";
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
@@ -5,6 +6,11 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
 	base: "./",
 	plugins: [react(), tailwindcss()],
+	resolve: {
+		alias: {
+			"~": path.resolve(__dirname, "src"),
+		},
+	},
 	server: {
 		port: 5173,
 		strictPort: true,
